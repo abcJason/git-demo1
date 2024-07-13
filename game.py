c722c7e203d1ce9ab73918b060042e7491bcb85d
jason <abcjason110@gmail.com>
2024-07-13 14:31:37 +0800

    猜數字第2版

--- game.py
+++ game.py
@@ -2,8 +2,7 @@ import random
 
 x = random.randint(1, 50)
 
-print(x)
-while x > 0:
+for i in range(5):
     y = int(input("1~50猜一個數字:"))
     if x == y:
         print("猜對了!!!")
@@ -11,8 +10,10 @@ while x > 0:
         break
     elif x > y:
         print("猜錯了~~")
-        print(f"50~{y}")
-
+        print("猜高一點了")
     else:
         print("猜錯了~~")
-        print(f"{y}~50")
+        print("猜低一點了")
+
+if X != y:
+    print(f"答案為{x}")
